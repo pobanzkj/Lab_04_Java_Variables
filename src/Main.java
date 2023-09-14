07630943dd683d866944fd5a2664a34e680821d3
pobanzkj <pobanzkj@mail.uc.edu>
2023-09-14 13:19:03 -0400

    send

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+        int intOperandA = 1;
+        int intOperandB = 6;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 1.75;
+        double doubleSum = doubleOperandA + doubleOperandB;
+
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+    }
+}
